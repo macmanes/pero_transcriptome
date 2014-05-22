3d71241a045c32152597f223f51d11dcb87bfcc6
Matt <macmanes@gmail.com>
2014-05-22 17:13:02 -0400

    hmming

---
+++ hamming1.py
@@ -0,0 +1,7 @@
+import distance
+import sys
+import os
+
+
+dist = distance.hamming(os.environ['var5'], os.environ['var6'])
+print dist
---
+++ hamming2.py
@@ -0,0 +1,7 @@
+import distance
+import sys
+import os
+
+
+dist = distance.hamming(os.environ['var8'], os.environ['var9'])
+print dist
