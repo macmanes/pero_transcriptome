3a7331357083398743fff05eebc6c0f16af5a546
Matt MacManes <macmanes@gmail.com>
2014-05-22 17:54:00 -0400

    BETTER ECHO

--- omega.sh
+++ omega.sh
@@ -74,7 +74,7 @@ while [ $n -lt $total ]; do
 						omegaMap $CF -outfile omega/om.$n.aln.out -fasta aligned/om.$n.aln &
 						let n=n+1
 					else
-						echo "HAMMING distanse is ZERO"
+						echo "HAMMING distanse is TOO BIG OR SMALL $var7"
 						let n=n+1
 					fi
 				else
@@ -90,13 +90,13 @@ while [ $n -lt $total ]; do
 					export var8=$(sed -n '2p' aligned/om.$n.aln)
 					export var9=$(sed -n '4p' aligned/om.$n.aln)
 					var10=$(python $HOME/pero_transcriptome/hamming2.py)
-					if [ $var10 -gt 0 ] && [ $var7 -lt 50 ];
+					if [ $var10 -gt 0 ] && [ $var10 -lt 50 ];
 						then
 						echo "HAMMING distanse is $var10"
 						omegaMap $CF -outfile omega/om.$n.aln.out -fasta aligned/om.$n.aln & #just do it!
 						let n=n+1
 					else
-						echo "HAMMING distanse is ZERO"        
+						echo "HAMMING distanse is TOO BIG OR SMALL $var10"        
 						let n=n+1
 					fi
 			else
