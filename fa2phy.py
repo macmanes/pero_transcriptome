325b63f310a38a6156ed04b3226ae6b74094026b
Matt <macmanes@gmail.com>
2014-05-22 17:10:19 -0400

    hmming

--- fa2phy.py
+++ fa2phy.py
@@ -2,4 +2,4 @@ from Bio import SeqIO
 import sys
 import os
 
-count = SeqIO.convert(sys.argv[1], "fasta", sys.argv[2], "phylip-relaxed")
+count = SeqIO.convert(sys.argv[1], "fasta", sys.argv[2], "phylip-sequential")
--- hamming.py
+++
@@ -1,6 +0,0 @@
-import distance
-import sys
-import os
-
-dist = distance.hamming(sys.argv[1], sys.argv[2])
-print dist
