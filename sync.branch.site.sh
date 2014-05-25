affee807219e6e05cf4973a10aa57d4dd0794ce0
Matt MacManes <macmanes@gmail.com>
2014-05-25 16:27:29 -0400

    let n=n+1

--- sync.branch.site.sh
+++ sync.branch.site.sh
@@ -7,4 +7,5 @@ while [ $n -lt $total ]; do
 	var2=$(grep lnL paml-branch/${n}.hits.out | awk '{print $5}')
 	var3=$(awk -v VAR1=$var1 -v VAR2=$var2 'BEGIN {print 2*(VAR1-VAR2) <0 ? -2*(VAR1-VAR2) : 2*(VAR1-VAR2)}')
 	echo -e $n.hits.out '\t' branch-site '\t' $var3 >> branch-site.txt
+	let n=n+1
 done
