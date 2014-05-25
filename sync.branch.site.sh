368dc60b91c4c96b1ae36afe44e44106e7b19da2
Matt MacManes <macmanes@gmail.com>
2014-05-25 16:25:36 -0400

    remove $i

--- sync.branch.site.sh
+++ sync.branch.site.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-total=13000
+total=1700
 n=1
 while [ $n -lt $total ]; do
 	var1=$(grep lnL paml/${n}.hits.out | awk '{print $5}')
 	var2=$(grep lnL paml-branch/${n}.hits.out | awk '{print $5}')
 	var3=$(awk -v VAR1=$var1 -v VAR2=$var2 'BEGIN {print 2*(VAR1-VAR2) <0 ? -2*(VAR1-VAR2) : 2*(VAR1-VAR2)}')
-	echo -e $i '\t' branch-site '\t' $var3 >> branch-site.txt
+	echo -e $n.hits.out '\t' branch-site '\t' $var3 >> branch-site.txt
 done
