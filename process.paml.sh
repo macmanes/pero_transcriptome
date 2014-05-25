0a2a9b5eedf96bac0e91cb41c4bea96ff93313a7
Matt MacManes <macmanes@gmail.com>
2014-05-25 13:07:19 -0400

    typo

--- process.paml.sh
+++ process.paml.sh
@@ -8,7 +8,7 @@ for i in `ls paml/*out`; do
 	var3=$(sed -n '4p' tmp)
 	var4=$(sed -n '5p' tmp)
 	var5=$(awk -v VAR1=$var1 -v VAR2=$var2 'BEGIN {print 2*(VAR1-VAR2) <0 ? -2*(VAR1-VAR2) : 2*(VAR1-VAR2)}')
-	var6=$(awk -v VAR1=$var3 -v VAR2=$var4 'BEGIN {print 2*(VAR3-VAR4) <0 ? -2*(VAR3-VAR4) : 2*(VAR3-VAR4)}')
+	var6=$(awk -v VAR3=$var3 -v VAR3=$var4 'BEGIN {print 2*(VAR3-VAR4) <0 ? -2*(VAR3-VAR4) : 2*(VAR3-VAR4)}')
 	echo -e $i '\t' M1a vs M2a '\t' $var5 >> M2.txt
 	echo -e $i '\t' M7 vs M8 '\t' $var6 >> M8.txt
 	var7=$(grep omega $i)
