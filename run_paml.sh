3cfd71004367241466d9a6362158514861952e7a
Matt MacManes <macmanes@gmail.com>
2014-05-27 06:50:44 -0400

    rat name processing

--- run_paml.sh
+++ run_paml.sh
@@ -40,9 +40,10 @@ while [ $n -lt $total ]; do
 			if [ ! -f $OD/$n.hits.out ] ; #have I done PAML?
 			then
 				sed -i 's_!_-_g' aligned/$n.hits.aln
-				sed -i "s/ENS.*/mus/g" aligned/$n.hits.aln
+				sed -i "s/ENSM.*/mus/g" aligned/$n.hits.aln
 				sed -i "s/gi.*/pema/g" aligned/$n.hits.aln
 				sed -i "s/[0-9].*/peer/g" aligned/$n.hits.aln
+				sed -i "s/ENSR.*/mus/g" aligned/$n.hits.aln
 				python $HOME/pero_transcriptome/fa2phy.py aligned/$n.hits.aln aligned/$n.hits.phy
 				sed -i "s_seqfile =.*_seqfile = aligned/${n}.hits.phy_g" codeml.ctl
 				sed -i "s_outfile =.*_outfile = ${OD}/${n}.hits.out_g" codeml.ctl
