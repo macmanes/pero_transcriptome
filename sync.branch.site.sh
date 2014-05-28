e375f177fe944890fa65034d007354c50e550d57
Matt MacManes <macmanes@gmail.com>
2014-05-28 12:38:30 -0400

    typo

--- sync.branch.site.sh
+++ sync.branch.site.sh
@@ -6,7 +6,7 @@ while [ $n -lt $total ]; do
 	var1=$(grep lnL null-model/${n}.hits.out | awk '{print $5}')
 	var2=$(grep lnL alt-model/${n}.hits.out | awk '{print $5}')
 	var0=$(grep 'foreground w' alt-model/${n}.hits.out | awk '{print $5}')
-	var3=$(awk -v VAR0=$var0 -v VAR1=$var1 -v VAR2=$var2 'BEGIN {print 2*(VAR1-VAR2) <0 ? -2*(VAR1-VAR2) : 2*(VAR1-VAR2)}')
+	var3=$(awk -v VAR1=$var1 -v VAR2=$var2 'BEGIN {print 2*(VAR1-VAR2) <0 ? -2*(VAR1-VAR2) : 2*(VAR1-VAR2)}')
 	echo -e $n.hits.out '\t' branch-site '\t' $var3 '\t' $var0 >> branch-site.txt
 	let n=n+1
 done
